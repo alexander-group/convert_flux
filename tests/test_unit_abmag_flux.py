ecf7c2e668b7bb4477739aa1d758cbeb49109225
Nayera Abdessalam <nayeramarie16@gmail.com>
2026-09-24 22:46:05 -0700

    test: improve unit tests for abmag flux conversion Includes astropy units and testing for conversion in both directions

--- tests/test_unit_abmag_flux.py
+++ tests/test_unit_abmag_flux.py
@@ -1,64 +1,54 @@
 from convert_flux import convert_flux
 import pytest
+import astropy.units as u
 
 
 @pytest.mark.xfail(reason="The convert_flux function is not implemented yet.")
 def test_flux_output_type():
-    abmag = 20.0
-    telescope = "SDSS"
-    bandpass = "r"
-    unit_in = "ABMag"
-    unit_out = "erg/s/cm^2"
-    flux = convert_flux(abmag, telescope, bandpass, unit_in, unit_out)
+    abmag = 20.0 * u.ABmag
+    unit_out = u.erg / (u.s * u.cm**2)
+    flux = convert_flux(abmag, unit_out)
 
-    assert isinstance(flux, float)
+    assert isinstance(flux, u.Quantity)
 
 
 @pytest.mark.xfail(reason="The convert_flux function is not implemented yet.")
-def test_abmag_to_flux_for_sdss_rband():
-    # Test case 1: Convert a known AB magnitude to flux
-    """
-    variables:
-    magnitude value
-    type of magnitude
-    telescope
-    bandpass
-    transmission curve
-    """
-    abmag = 20.0
-    telescope = "SDSS"
-    bandpass = "r"
-    unit_in = "ABMag"
-    unit_out = "erg/s/cm^2"
-    flux = convert_flux(abmag, telescope, bandpass, unit_in, unit_out)
+def test_abmag_to_flux_and_back_to_ab_mag_for_sdss_rband():
+    expected_abmag = 20.0 * u.ABmag
+    expected_flux = 3.22e-14 * u.erg / (u.s * u.cm**2)
 
-    expected_flux = 3.22e-14
+    unit_out_flux = u.erg / (u.s * u.cm**2)
+    unit_out_abmag = u.ABmag
+
+    transmission_curve = "sdss_r"
+
+    flux = convert_flux(expected_abmag, unit_out_flux, transmission_curve)
+    abmag = convert_flux(expected_flux, unit_out_abmag, transmission_curve)
 
     assert (
         abs(flux - expected_flux) < 1e-15
     ), f"Expected {expected_flux}, but got {flux}"
+    assert (
+        abs(abmag - expected_abmag) < 1e-15
+    ), f"Expected {expected_abmag}, but got {abmag}"
 
 
 @pytest.mark.xfail(reason="The convert_flux function is not implemented yet.")
-def test_abmag_to_flux_for_ztf_rband():
-    # Test case 2: Convert a known AB magnitude to flux
-    """
-    variables:
-    magnitude value
-    type of magnitude
-    telescope
-    bandpass
-    transmission curve
-    """
-    abmag = 20.0
-    telescope = "ZTF"
-    bandpass = "r"
-    unit_in = "ABMag"
-    unit_out = "erg/s/cm^2"
-    flux = convert_flux(abmag, telescope, bandpass, unit_in, unit_out)
+def test_abmag_to_flux_and_back_to_ab_mag_for_ztf_rband():
+    expected_abmag = 20.0 * u.ABmag
+    expected_flux = 4.08e-14 * u.erg / (u.s * u.cm**2)
 
-    expected_flux = 4.08e-14
+    unit_out_flux = u.erg / (u.s * u.cm**2)
+    unit_out_abmag = u.ABmag
+
+    transmission_curve = "ztf_r"
+
+    flux = convert_flux(expected_abmag, unit_out_flux, transmission_curve)
+    abmag = convert_flux(expected_flux, unit_out_abmag, transmission_curve)
 
     assert (
         abs(flux - expected_flux) < 1e-15
     ), f"Expected {expected_flux}, but got {flux}"
+    assert (
+        abs(abmag - expected_abmag) < 1e-15
+    ), f"Expected {expected_abmag}, but got {abmag}"
